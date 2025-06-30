07deab70e7fe1da62e3a0fed4233e9ffa0ae27de
waynemorphic <wayneklyde@gmail.com>
2025-06-30 14:55:59 +0300

    refactor: Added ICU module to i18n instance

--- src/email/i18n.ts
+++ src/email/i18n.ts
@@ -2,6 +2,8 @@ import { GetMessages } from "keycloakify-emails";
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enTranslation from "./locales/en/translation.json";
+import ICU from "i18next-icu";
+import LanguageDetector from "i18next-browser-languagedetector";
 
 const resources = {
     en: {
@@ -9,11 +11,15 @@ const resources = {
     }
 };
 
-i18n.use(initReactI18next).init({
-    resources,
-    lng: "en",
-    interpolation: { escapeValue: false }
-});
+i18n.use(ICU)
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+        resources,
+        fallbackLng: "en",
+        debug: true,
+        interpolation: { escapeValue: false }
+    });
 
 export default i18n;
 
