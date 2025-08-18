2d1b4eb55654048316cf0dad36ebdd0494cae711
waynemorphic <wayneklyde@gmail.com>
2025-08-18 12:02:51 +0300

    refactor: Updated translations for different locales

--- src/email/i18n.ts
+++ src/email/i18n.ts
@@ -5,7 +5,7 @@ import enTranslation from "./locales/en/translation.json";
 import esTranslation from "./locales/es/translation.json";
 import deTranslation from "./locales/de/translation.json";
 import ruTranslation from "./locales/ru/translation.json";
-import czTranslation from "./locales/cz/translation.json";
+import czTranslation from "./locales/cs/translation.json";
 import frTranslation from "./locales/fr/translation.json";
 import itTranslation from "./locales/it/translation.json";
 import ICU from "i18next-icu";
@@ -24,7 +24,7 @@ const resources = {
     ru: {
         translation: ruTranslation
     },
-    cz : {
+    cs : {
         translation: czTranslation
     },
     fr: {
@@ -35,7 +35,8 @@ const resources = {
     }
 };
 
-i18n.use(ICU)
+if (!i18n.isInitialized) {
+    i18n.use(ICU)
     .use(initReactI18next)
     .use(LanguageDetector)
     .init({
@@ -44,12 +45,25 @@ i18n.use(ICU)
         debug: true,
         interpolation: { escapeValue: false },
     });
+}
 
 export default i18n;
 
 export const getMessages: GetMessages = props => {
     const t = i18n.getFixedT(props.locale);
     return {
-        "requiredAction.WELCOME": t('execute-actions.invitation')
+        "requiredAction.CONFIGURE_TOTP": t('execute-actions.configureTOTP'),
+        "requiredAction.UPDATE_PASSWORD": t('execute-actions.updatePassword'),
+        "requiredAction.UPDATE_PROFILE": t('execute-actions.updateProfile'),
+        "requiredAction.VERIFY_EMAIL": t('execute-actions.verifyEmail'),
+        "requiredAction.CONFIGURE_RECOVERY_AUTHN_CODES": t('execute-actions.recoveryAuthenticationCodes'),
+        "requiredAction.LINKING_IDP": t('execute-actions.linkingIdentityProvider'),
+        "requiredAction.webauthn-register": t('execute-actions.webAuthnRegister'),
+        "requiredAction.webauthn-register-passwordless": t('execute-actions.webAuthnRegisterPasswordless'),
+        "requiredAction.delete_credential": t('execute-actions.deleteCredential'),
+        "requiredAction.recovery-authn-codes": t('execute-actions.recoveryAuthenticationCodes'),
+        "requiredAction.update_user_locale": t('execute-actions.updateUserLocale'),
+
+        "requiredAction.DEFAULT": t('execute-actions.welcome'),
     }
 };
