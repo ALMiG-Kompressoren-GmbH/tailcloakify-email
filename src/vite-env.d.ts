4521bee3f766006b73f0913988bfe8146819b6b7
waynemorphic <wayneklyde@gmail.com>
2025-09-18 13:44:32 +0300

    fix: Renamed environment variable and removed unnecessary alt variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="vite/client" />
-declare const TAILCLOAKIFY_BACKGROUND_EMAIL_IMAGE_URL: string;
+declare const TAILCLOAKIFY_EMAIL_BACKGROUND_IMAGE_URL: string;
 declare const TAILCLOAKIFY_EMAIL_LOGO: string;
