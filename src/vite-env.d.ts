21bf8499a6ac0f2c49482ff059905161330618b0
waynemorphic <wayneklyde@gmail.com>
2025-09-26 16:22:46 +0300

    chore: Administrator email address is not retrievable from Keycloak. Added contact email address env variable.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,4 @@
 declare const TAILCLOAKIFY_EMAIL_BACKGROUND_IMAGE_URL: string;
 declare const TAILCLOAKIFY_EMAIL_LOGO: string;
 declare const TAILCLOAKIFY_EMAIL_FONT_FAMILY: string;
+declare const TAILCLOAKIFY_CONTACT_EMAIL: string;
